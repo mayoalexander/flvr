47237d77200f564de369dea7894960917dc6bcb2
Alexander Mayo <admin@freelabel.net>
2016-10-30 15:16:53 -0500

    you can now see the posts on your categories

--- lvtr/views/widgets/view_category.php
+++ lvtr/views/widgets/view_category.php
@@ -5,10 +5,21 @@ $category = $site->get_category_by_id($_GET['category_id']);
 
 $posts = $site->get_category_posts($category['name'],$_GET['user_name']);
 
-// $site->debug($posts,1);
+
 // exit;
 ?>
 <h1 class="page-header"><?php echo $category['name']; ?></h1>
-<?php //echo $site->display_media_grid($posts,$_SESSION['user_name'],0); ?>
+<?php 
+	if ($posts===NULL) {
+		echo 'No posts have been added!';
+	} else {
+		foreach ($posts as $key => $post) {
+			$post_data = $site->get_post_by_id($post['post_id']);
+			$image = '<img src="'.$post_data['photo'].'" height="50px"/>';
+			echo $image.$post_data['twitter'].': '.$post_data['blogtitle'].'<hr>';
+		}
+		// echo $site->display_media_grid($posts,$_SESSION['user_name'],0); 	
+	}
+?>
 
 <script type="text/javascript" src="<?php echo $site->url ?>js/dashboard.js"></script>
