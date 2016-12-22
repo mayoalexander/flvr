d465b95c78109c46e4fa217f5069f995855c86bd
Alexander Mayo <mayoalexandertd@outlook.com>
2016-12-21 21:40:16 -0600

    added TV RADIO MAG to public folder for subdirectory access

---
+++ public/mag/index.php
@@ -0,0 +1,19 @@
+<?php
+// include '../../lvtr/header.php';
+// $site = new Config();
+
+// var_dump(scandir('../../'));
+
+// echo $_SERVER['SCRIPT_NAME']
+?>
+
+<style type="text/css">
+	body, html {
+		margin:0;
+		padding:0;
+	}	
+	.frame-window {
+		width:100vw;height: 100vh;border:none;
+	}
+</style>
+<iframe src="http://freelabel.net/view/mag/" class="frame-window"></iframe>
