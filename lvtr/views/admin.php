ef857e5f73dfe717410456a7a6a939007028b4e4
Alex Mayo <mayoalexandertd@icloud.com>
2017-02-06 13:11:46 -0600

    finished magazine and TV apps

--- lvtr/views/admin.php
+++ lvtr/views/admin.php
@@ -8,8 +8,55 @@ $site = new Config();
 	<h1>Admin</h1>
 	<hr>
 
+  <nav class="navbar navbar-inverse widget-navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">ADMIN</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
 
-  <nav class="navbar navbar-inverse">
+          <!-- CONTENT -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Content <span class="caret"></span></a>
+            <ul class="dropdown-menu ">
+              <li><a href="#" data-page="RSS">RSS</a></li>
+              <li><a href="#" data-page="widgets/submissions">Submissions</a></li>
+              <li><a href="#" data-page="Automation">Automation</a></li>
+              <li><a href="#" data-page="Analytics">Analytics</a></li>
+              <li><a href="#" data-page="Promos">Promos</a></li>
+            </ul>
+          </li>
+          
+          <!-- CONTENT -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Clients <span class="caret"></span></a>
+            <ul class="dropdown-menu ">
+              <li><a href="#" data-page="Clients">Clients</a></li>
+              <li><a href="#" data-page="Twitter">Twitter</a></li>
+              <li><a href="#" data-page="Emailer">Emailer</a></li>
+              <li><a href="#" data-page="Script">Script</a></li>
+              <li><a href="#" data-page="SOM">SOM</a></li>
+              <li><a href="#" data-page="Leads">Leads</a></li>
+              <li><a href="#" data-page="Analytics">Analytics</a></li>
+              <li><a href="#" data-page="October">October</a></li>
+              <li><a href="#" data-page="Promos">Promos</a></li>
+            </ul>
+          </li>
+
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+  </nav>
+
+
+<!--   <nav class="navbar navbar-inverse">
     <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapsex" data-target=".admin-nav">
@@ -18,10 +65,11 @@ $site = new Config();
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Project name</a>
+        <a class="navbar-brand" href="#">ADMIN</a>
       </div>
       <div class="admin-nav collapsex">
         <ul class="nav navbar-nav widget-navigation">
+          <li class="dropdown"><span href="#" data-page="RSS">Content</a></span>
           <li><a href="#" data-page="RSS">RSS</a></li>
           <li><a href="#" data-page="Clients">Clients</a></li>
           <li><a href="#" data-page="Twitter">Twitter</a></li>
@@ -35,9 +83,9 @@ $site = new Config();
           <li><a href="#" data-page="October">October</a></li>
           <li><a href="#" data-page="Promos">Promos</a></li>
         </ul>
-      </div><!--/.nav-collapse -->
+      </div>
     </div>
-  </nav>
+  </nav> -->
   <hr>
 </section>
 
