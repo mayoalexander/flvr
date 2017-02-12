72c418c16166830212a6337ba8d53c374d3f4c53
Alexander Mayo <admin@freelabel.net>
2017-02-11 19:41:02 -0600

    added CSS + updated mag page data

--- lvtr/views/analytics.php
+++ lvtr/views/analytics.php
@@ -5,6 +5,7 @@ $site = new Config();
 $leads = $site->get_leads('today');
 // $site->debug($leads,1);
 
+/* COMPILE LEAD DATA */
 foreach ($leads as $key => $lead) {
 	$leads_compiled[$lead['lead_twitter']][] = $lead['lead_name'];
 }
@@ -12,84 +13,134 @@ $num_of_leads = count($leads_compiled);
 $num_of_som = count($site->get_som('today'));
 $num_of_new_clients = count($site->get_new_clients('today'));
 
-// $site->debug($num_of_new_clients,1);
-?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.js"></script>
+/* COMPLIE POSTS */
 
-<div class="container row">
+
+/* STATIC VARIALBES */
+$cost = 45;
+$estimates = '$'.($num_of_leads * $cost);
+
+/* PERCENTAGES */
+$som_percentage = round(($num_of_som / 24) * 100);
+$client_percentages = round(($num_of_new_clients / 10) * 100);
+
+?>
+
+<div class="container">
 	<h1>Analytics</h1>
-	<div class="col-md-2">
+	<div class="col-md-3">
 		<h3>Leads Today</h3>
 	</div>
-	<div class="col-md-2">
+	<div class="col-md-3">
 		<h3>SOM Today</h3>
 	</div>
-	<div class="col-md-2">
+	<div class="col-md-3">
 		<h3>New Clients</h3>
 	</div>
-</div>
-<div class="container row">
-	<div class="col-md-2">
-		<?php echo $num_of_leads; ?>/100
-	</div>
-	<div class="col-md-2">
-		<?php echo $num_of_som; ?>
-	</div>
-	<div class="col-md-2">
-		<?php echo $num_of_new_clients; ?>
-	</div>
-	<div class="col-md-6">
-		<canvas id="myChart" width="400" height="400"></canvas>
-	</div>
-	<div class="col-md-6">
-		<canvas id="pieChart" width="400" height="400"></canvas>
+	<div class="col-md-3">
+		<h3>Estimates</h3>
 	</div>
 </div>
+<div class="container">
+	<div class="col-md-3">
+		<h2><?php echo $num_of_leads; ?>/100</h2>
+	</div>
+	<div class="col-md-3">
+		<h2><?php echo $num_of_som; ?></h2>
+	</div>
+	<div class="col-md-3">
+		<h2><?php echo $num_of_new_clients; ?></h2>
+	</div>
+	<div class="col-md-3">
+		<h2><?php echo $estimates; ?></h2>
+	</div>
+</div>
+
+
+<section class="container">
+	<div class="col-md-12">
+		<h2 class="page-header">Leads <small><?php echo $num_of_leads; ?>%</small></h2>
+		<div class="progress">
+		  <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $num_of_leads; ?>"
+		  aria-valuemin="0" aria-valuemax="<?php echo $num_of_leads; ?>" style="width:<?php echo $num_of_leads; ?>%">
+		    <span class="sr-only"><?php echo $num_of_leads; ?>% Complete</span>
+		  </div>
+		</div>
+	</div>
+	<div class="col-md-12">
+		<h2 class="page-header">SOMs <small><?php echo $som_percentage; ?>%</small></h2>
+		<div class="progress">
+		  <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $som_percentage; ?>"
+		  aria-valuemin="0" aria-valuemax="<?php echo $som_percentage; ?>" style="width:<?php echo $som_percentage; ?>%">
+		    <span class="sr-only"><?php echo $som_percentage; ?>% Complete</span>
+		  </div>
+		</div>
+	</div>
+	<div class="col-md-12">
+		<h2 class="page-header">Clients <small><?php echo $client_percentages; ?>%</small></h2>
+		<div class="progress">
+		  <div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $client_percentages; ?>"
+		  aria-valuemin="0" aria-valuemax="<?php echo $client_percentages; ?>" style="width:<?php echo $client_percentages; ?>%">
+		    <span class="sr-only"><?php echo $client_percentages; ?>% Complete</span>
+		  </div>
+		</div>
+	</div>
+</section>
 
 
 
 
 
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.js"></script>
 <script>
 
+$(function(){
 
-function initializeChart(chart) {
-	var ctx = document.getElementById(chart);
-	var dataset = ['<?php echo ($num_of_leads * 0.1); ?>','<?php echo $num_of_new_clients; ?>','<?php echo ($num_of_som/4); ?>',]
-	var myChart = new Chart(ctx, {
-	    type: 'doughnut',
-	    data: {
-	        labels: ['Leads',"New Clients", "SOM Deploys"],
-	        datasets: [{
-	            label: '# of Leads Today',
-	            data: dataset,
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(255, 159, 64, 0.2)',
-	                'pink'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        // scales: {
-	        //     yAxes: [{
-	        //         ticks: {
-	        //             beginAtZero:true
-	        //         }
-	        //     }]
-	        // }
-	    }
-	});
-}
 
-initializeChart('myChart','bar');
-initializeChart('pieChart', 'pie');
+// var myLineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: data,
+//     options: options
+// });
+
+	function initializeChart(chart) {
+		var ctx = document.getElementById(chart);
+		var dataset = ['<?php echo ($num_of_leads * 0.1); ?>','<?php echo $num_of_new_clients; ?>','<?php echo ($num_of_som/4); ?>',]
+		var myChart = new Chart(ctx, {
+		    type: 'doughnut',
+		    data: {
+		        labels: ['Leads',"New Clients", "SOM Deploys"],
+		        datasets: [{
+		            label: '# of Leads Today',
+		            data: dataset,
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 0.2)',
+		                'rgba(255, 159, 64, 0.2)',
+		                'pink'
+		            ],
+		            borderColor: [
+		                'rgba(255,99,132,1)',
+		                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        // scales: {
+		        //     yAxes: [{
+		        //         ticks: {
+		        //             beginAtZero:true
+		        //         }
+		        //     }]
+		        // }
+		    }
+		});
+	}
+
+	// initializeChart('myChart','bar');
+	// initializeChart('pieChart', 'pie');	
+});
+
 
 </script>
---
+++ view/plugins/freelabel/freelabel/updates/builder_table_update_freelabel_freelabel_packages_4.php
@@ -0,0 +1,23 @@
+<?php namespace Freelabel\Freelabel\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateFreelabelFreelabelPackages4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('freelabel_freelabel_packages', function($table)
+        {
+            $table->string('slug')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('freelabel_freelabel_packages', function($table)
+        {
+            $table->dropColumn('slug');
+        });
+    }
+}
